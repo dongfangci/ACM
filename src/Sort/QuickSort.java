ab491f36e0484c887722044c4112eb94972ae9de
dongfangci <zhangdong9105@gmail.com>
2016-06-23 17:22:07 +0800

    first

---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,84 @@
+package Sort;
+
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class QuickSort {
+	Random random = new Random();
+	public void quickSort(int[] a){
+		if(a == null)
+			throw new NullPointerException("Null");
+		quickSort(a, 0, a.length-1);
+	}
+	
+
+	
+	private void quickSort(int[] a, int lo, int hi){
+		if(lo >= hi)
+			return;
+		int par = partition2(a, lo, hi);
+		quickSort(a, lo, par-1);
+		quickSort(a, par+1, hi);
+	}
+
+	private int partition(int[] a, int lo, int hi) {
+		int i = lo;
+		int j = hi + 1;
+		int tmp = a[lo];
+		while(true){
+			while(a[++i] < tmp) if(i == hi) break;
+			while(a[--j] > tmp) if(j == lo) break;
+			if(i >= j)
+				break;
+			swap(a, i, j);
+		}
+		swap(a,lo,j);
+		return j;
+	}
+	
+	private int partition2(int[] a, int lo, int hi) {
+		int i = lo;
+		int j = hi + 1;
+		int rm = random.nextInt(hi - lo) + lo;
+		swap(a,lo,rm);
+		int tmp = a[lo];
+		System.out.println(rm);
+		while(true){
+			while(a[++i] < tmp) if(i == hi) break;
+			while(a[--j] > tmp) if(j == lo) break;
+			if(i >= j)
+				break;
+			swap(a, i, j);
+		}
+		swap(a,lo,j);
+		return j;
+	}
+
+	private void swap(int[] a, int i, int j) {
+		int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	public static void main(String[] args) {
+		int[] a = new int[15];
+		//int[] a = null;
+		for(int i = 0; i < a.length; i++){
+			a[i] =(int) (Math.random()*10);
+		}
+		for(int i = 0; i < a.length; i++){
+			System.out.print(a[i] + " ");
+		}
+		System.out.println(" ");
+		
+		QuickSort q1 = new QuickSort();
+		q1.quickSort(a);
+		for(int i = 0; i < a.length; i++){
+			System.out.print(a[i] + " ");
+		}
+		System.out.println(" ");
+		
+	}
+
+}
