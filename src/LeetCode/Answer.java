25b88987d8d0f4d353db5c626417fc6f6d9d045d
dongfangci <zhangdong9105@gmail.com>
2016-06-28 21:52:41 +0800

    TreeOrder
    Signed-off-by: dongfangci-PC

--- src/LeetCode/Answer.java
+++ src/LeetCode/Answer.java
@@ -1,5 +1,15 @@
 package LeetCode;
-public class Answer {}
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Answer {
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+}
 
 /*
   136. Single Number
@@ -28,3 +38,60 @@ public class Answer {}
 	a xor b xor a = a xor a xor b = 0 xor b = b. 
 
  * */
+	
+	
+/*145.
+ * Preorder, Inorder and Postorder Traversal Iterative Java Solution
+ * 
+ * 
+ * 
+ * */
+	public List<Integer> preorderTraversal(TreeNode root) {
+	    List<Integer> list = new ArrayList<>();
+	    if(root == null) return list;
+	    Stack<TreeNode> stack = new Stack<>();
+	    stack.push(root);
+	    while(!stack.empty()){
+	        root = stack.pop();
+	        list.add(root.val);
+	        if(root.right != null) stack.push(root.right);
+	        if(root.left != null) stack.push(root.left);
+	    }
+	    return list;
+	}
+	
+	public List<Integer> inorderTraversal(TreeNode root) {
+	    List<Integer> list = new ArrayList<>();
+	    if(root == null) return list;
+	    Stack<TreeNode> stack = new Stack<>();
+	    while(root != null || !stack.empty()){
+	        while(root != null){
+	            stack.push(root);
+	            root = root.left;
+	        }
+	        root = stack.pop();
+	        list.add(root.val);
+	        root = root.right;
+	    }
+	    return list;
+	}
+	
+	public List<Integer> postorderTraversal(TreeNode root) {
+	    List<Integer> list = new ArrayList<>();
+	    if(root == null) return list;
+	    Stack<TreeNode> stack = new Stack<>();
+	    stack.push(root);
+	    while(!stack.empty()){
+	        root = stack.pop();
+	        list.add(0, root.val);
+	        if(root.left != null) stack.push(root.left);
+	        if(root.right != null) stack.push(root.right);
+	    }
+	    return list;
+	}
+	
+	
+	
+	
+	
+}
