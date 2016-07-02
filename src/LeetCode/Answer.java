b72b73bb1c97c6f5bc27fb8259685d75f94c6565
dongfangci <zhangdong9105@gmail.com>
2016-07-02 11:29:40 +0800

    test2

--- src/LeetCode/Answer.java
+++ src/LeetCode/Answer.java
@@ -1,7 +1,9 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 public class Answer {
@@ -40,7 +42,7 @@ class TreeNode{
  * */
 	
 	
-/*145.
+/*144. / 145. TreeOrder
  * Preorder, Inorder and Postorder Traversal Iterative Java Solution
  * 
  * 
@@ -90,8 +92,77 @@ class TreeNode{
 	    return list;
 	}
 	
+	 public List<Integer> postorderTraversal2(TreeNode root) {	//更加一般的解法，同样适用于中序遍历
+	       List<Integer> list = new ArrayList<Integer>();
+	       if(root == null)
+	       return list;
+	       Stack<TreeNode> s = new Stack<TreeNode>();
+	       HashMap<TreeNode,Integer> map = new HashMap<TreeNode,Integer>();
+	       TreeNode p = null;
+	       s.push(root);
+	       map.put(root,0);
+	       while(!s.isEmpty()){
+	           p = s.pop();
+	           
+	          if(map.get(p) == 0){				//	第一次压栈
+	            s.push(p);
+	            map.put(p,1);					//第二次压栈
+	            if(p.right != null){
+	             s.push(p.right);
+	            map.put(p.right,0);
+	            }
+	            if(p.left != null){
+	            s.push(p.left);
+	            map.put(p.left,0);       
+	            }
+	          }
+	         else
+	         list.add(p.val);
+	          
+	       }
+	       return list;
+	    }
+/*
+ * 104. Maximum Depth of Binary Tree
+ * Given a binary tree, find its maximum depth.
+   The maximum depth is the number of nodes along the longest 
+   path from the root node down to the farthest leaf node.
+ * */ 
+ 
 	
-	
-	
+	 public int maxDepth(TreeNode root) {
+	        if (root == null) {
+	            return 0;
+	        }
+	        Map<TreeNode, Integer> depthMap = new HashMap<>();
+	        depthMap.put(root, 1);
+	        int maxDepth = 1;
+	        Stack<TreeNode> stack = new Stack<>();
+	        stack.push(root);
+	        while (!stack.isEmpty()) {
+	            root = stack.pop();
+	            int depth = depthMap.get(root);
+	            maxDepth = Math.max(maxDepth, depthMap.get(root));
+	            if (root.right != null) {
+	                depthMap.put(root.right, depth + 1);
+	                stack.push(root.right);
+	            }
+	            if (root.left != null) {
+	                depthMap.put(root.left, depth + 1);
+	                stack.push(root.left);
+	            }
+	            depthMap.remove(root);
+	        }
+	        return maxDepth;
+	    }
+	 
+	 public int maxDepth2(TreeNode root) {
+	        if (root == null) {
+	            return 0;
+	        }
+	        int left = maxDepth2(root.left);
+	        int right = maxDepth2(root.right);
+	        return((left > right) ? left + 1 : right + 1);
+	 }
 	
 }
---
+++ src/Test/Test.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		char a = 'a';
+		char b = 'a';
+		char c = 'b';
+		System.out.println((int)a);
+		System.out.println((int)b);
+		System.out.println((int)c);
+		System.out.println(97^98);
+		System.out.println(a^b);
+		System.out.println(a^c);
+	}
+
+}
